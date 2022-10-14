d57fe978d3016c85c235521f0d10e46556c6a49f
jakemarkd3v <jakemarkd3v@gmail.com>
2022-10-15 06:53:15 +0800

    added files

---
+++ ex01.py
@@ -0,0 +1,4 @@
+#write a program that prints your name 100 times
+
+for i in range(100):
+    print("Jake")
---
+++ ex02.py
@@ -0,0 +1,5 @@
+while True:
+    username = input("Enter your name: ")
+    for i in range(10):
+        print(f"{username}", end=" ")
+    break
